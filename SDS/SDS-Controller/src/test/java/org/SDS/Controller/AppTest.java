3b6cedc8e4aab9eecc0ef5941d07c5c270ba0885
aiglesias <aiglesias@C0LAP56.afyt.ing>
2021-10-25 17:17:16 +0200

    Añadidas anotaciones en el test de prueba del controller

--- SDS/SDS-Controller/src/test/java/org/SDS/Controller/AppTest.java
+++ SDS/SDS-Controller/src/test/java/org/SDS/Controller/AppTest.java
@@ -3,18 +3,22 @@ package org.SDS.Controller;
 import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import com.sds.app.SDSApplication;
 
 /**
  * Unit test for simple App.
  */
-public class AppTest 
-{
-    /**
-     * Rigorous Test :-)
-     */
-    @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
-    }
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = SDSApplication.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class AppTest {
+
+	@Test
+	public void shouldAnswerWithTrue() {
+		assertTrue(true);
+	}
+
 }
