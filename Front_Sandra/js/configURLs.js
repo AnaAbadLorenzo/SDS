fa5150535589b3efca3836d9e629cb81415ab3d7
Ana Abad Lorenzo <88203308+AnaAbadLorenzo@users.noreply.github.com>
2022-03-05 18:31:17 +0100

    Front

---
+++ Front_Sandra/js/configURLs.js
@@ -0,0 +1,3 @@
+var urlPeticionAjaxLogin = "http://localhost:8090/login/login";
+var urlPeticionAjaxRegistro = "http://localhost:8090/registrar";
+var urlPeticionAjaxRecuperarPass = "http://localhost:8090/login/recuperarPass"
