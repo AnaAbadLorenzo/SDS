0d69e1a98a117eb5195e8928a66d460b127e799b
Ana Abad Lorenzo <88203308+AnaAbadLorenzo@users.noreply.github.com>
2022-03-05 18:34:09 +0100

    Subidos js

---
+++ Front_Sandra/js/login.js
@@ -0,0 +1,115 @@
+/**
+ * Ana Abad Lorenzo
+ * 
+ * Funciones para login.html
+ * 
+ */
+
+/**Función login ajax con promesas*/
+function loginAjaxPromesa(){
+  return new Promise(function(resolve, reject) {
+    var data = {
+      usuario : $('#userLogin').val(),
+      passwdUsuario : $('#passLogin').val()
+    }
+    
+    $.ajax({
+      method: "POST",
+      url: urlPeticionAjaxLogin,
+      contentType : "application/json",
+      data: JSON.stringify(data),  
+      dataType : 'json',
+      }).done(res => {
+          if (res.code != 'LOGIN_OK') {
+            reject(res);
+          }
+          resolve(res);
+    }).fail(errorGenerico());
+  });
+}
+
+/**Función recuperar pass ajax con promesas*/
+function recuperarPassAjaxPromesa(){
+  return new Promise(function(resolve, reject) {
+    var data = {
+      usuario : $('#userRecuperarPass').val(),
+      emailUsuario : $('#emailUser').val()
+    }
+    
+    $.ajax({
+      method: "POST",
+      url: urlPeticionAjaxRecuperarPass,
+      contentType : "application/json",
+      data: JSON.stringify(data),  
+      dataType : 'json',
+      }).done(res => {
+          if (res.code != 'RECUPERAR_PASS_OK') {
+            reject(res);
+          }
+          resolve(res);
+    }).fail(errorGenerico());
+  });
+}
+
+/**Funcion para mostrar la modal de errores**/
+function errorGenerico() {
+  $("#mensajeError").removeClass();
+  $("#mensajeError").addClass('02109');
+  document.getElementById("modal").style.display = "block";
+}
+
+async function login() {
+  await loginAjaxPromesa()
+    .then((res) => {
+        setCookie('tokenUsuario',res.data.tokenUsuario);
+        setCookie('rolUsuario', res.data.rolUsuario);
+        setCookie('usuario', usuario);
+        window.location.href = "menu.html";
+    })
+    
+    .catch((res) => {
+        $('#login-modal').modal('toggle'); 
+        $("#modal-title").removeClass();
+        $("#modal-title").addClass("ERROR");
+        $("#mensajeError").removeClass();
+        $("#mensajeError").addClass(res.code);
+
+        let idElementoList = ["userLogin", "passLogin"];
+        resetearFormulario("formularioLogin", idElementoList);
+        setLang(getCookie('lang'));
+        document.getElementById("modal").style.display = "block";
+  });
+}
+
+async function recuperarPass(){
+  await recuperarPassAjaxPromesa()
+  .then((res) => {
+      $('#recuperarcontrasena-modal').modal('toggle');
+      $('#login-modal').modal('toggle'); 
+      $("#modal-title").removeClass();
+      $("#modal-title").addClass("CONTRASEÑA_REC_OK");
+      $("#mensajeError").removeClass();
+      $("#mensajeError").addClass(res.code);
+
+      let idElementoList = ["userRecuperarPass", "emailUser"];
+      resetearFormulario("formularioRecuperarPass", idElementoList);
+      setLang(getCookie('lang'));
+      document.getElementById("modal").style.display = "block";
+
+
+    })
+    
+    .catch((res) => {
+        $('#recuperarcontrasena-modal').modal('toggle'); 
+        $('#login-modal').modal('toggle');
+        $("#modal-title").removeClass();
+        $("#modal-title").addClass("ERROR"); 
+        $("#mensajeError").removeClass();
+        $("#mensajeError").addClass(res.code);
+        let idElementoList = ["userRecuperarPass", "emailUser"];
+        resetearFormulario("formularioRecuperarPass", idElementoList);
+        setLang(getCookie('lang'));
+        document.getElementById("modal").style.display = "block";
+    });
+}
+        
