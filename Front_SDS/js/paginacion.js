9c4634c818988031cd242e59745065c0d2985756
Sandra <aicuna87@gmail.com>
2022-05-09 18:00:18 +0200

    Correcciones front y ordenación de funcionalidades paa el menu

--- Front_SDS/js/paginacion.js
+++ Front_SDS/js/paginacion.js
@@ -45,7 +45,7 @@ function paginador(totalResults, funcionalidad, entidad){
 
     var paginacionPrevio = '<nav aria-label= "Page navigation example">' +
     					'<ul class="pagination">' +
-    						'<li id="anterior" class="page-item disabled tooltip">' +
+    						'<li id="anterior" class="page-item disabled tooltip6">' +
     							'<a class="page-link" href="#" onclick="cargarPosicion(' + posActual + ', \'ANTERIOR\', \'' + entidad + '\')" aria-label="Previous">' +
     								'<span aria-hidden="true">&laquo;</span>' +
     								'<span class="sr-only">Previous</span>' +
@@ -55,7 +55,7 @@ function paginador(totalResults, funcionalidad, entidad){
 
     paginas = escogeEntidadPaginacion(entidad, funcionalidad);
 
-   	var paginacionSiguiente = '<li id="siguiente" class="page-item navegacion tooltip">' +
+   	var paginacionSiguiente = '<li id="siguiente" class="page-item navegacion tooltip6">' +
 	   								'<a class="page-link" href="#" onclick="cargarPosicion(' + posActual + ', \'SIGUIENTE\', \'' + entidad + '\')" aria-label="Next">' + 
 	   									'<span aria-hidden="true">&raquo;</span>' + 
 	   									'<span class="sr-only">Next</span>' +
