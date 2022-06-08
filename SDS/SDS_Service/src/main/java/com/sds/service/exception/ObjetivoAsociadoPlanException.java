5e4c0d37b17d2b741765bf00e7d3a5f7d1331b6b
Jrodeiro <Jrodeiro@DESKTOP-GU8FSO0>
2022-06-08 10:02:34 +0200

    añadir id serializado

--- SDS/SDS_Service/src/main/java/com/sds/service/exception/ObjetivoAsociadoPlanException.java
+++ SDS/SDS_Service/src/main/java/com/sds/service/exception/ObjetivoAsociadoPlanException.java
@@ -2,6 +2,8 @@ package com.sds.service.exception;
 
 public class ObjetivoAsociadoPlanException extends Exception {
 
+	private static final long serialVersionUID = 2838379553280333413L;
+
 	public ObjetivoAsociadoPlanException(final String code, final String msg) {
 		super(msg);
 	}
