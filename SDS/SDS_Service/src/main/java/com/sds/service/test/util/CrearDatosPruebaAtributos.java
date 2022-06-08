a7d149a22ade8b4467ad887e5c103d4fde0146c7
Jrodeiro <Jrodeiro@DESKTOP-GU8FSO0>
2022-06-08 12:10:33 +0200

    Añadir log y modificar validaciones ñ y acentos

--- SDS/SDS_Service/src/main/java/com/sds/service/test/util/CrearDatosPruebaAtributos.java
+++ SDS/SDS_Service/src/main/java/com/sds/service/test/util/CrearDatosPruebaAtributos.java
@@ -1,8 +1,13 @@
 package com.sds.service.test.util;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import com.sds.service.test.model.DatosPruebaAtributos;
 
 public class CrearDatosPruebaAtributos {
+
+	public final static Logger LOGGER = LoggerFactory.getLogger(CrearDatosPruebaAtributos.class);
 
 	public DatosPruebaAtributos createDatosPruebaAtributos(final String resultadoObtenido,
 			final String resultadoEsperado, final String prueba, final String tipoPrueba, final String valor,
@@ -18,6 +23,13 @@ public class CrearDatosPruebaAtributos {
 			datosPruebaAtributos.setResultadoObtenido(resultadoObtenido);
 			datosPruebaAtributos.setTipoPrueba(tipoPrueba);
 		}
+
+		LOGGER.debug("Resultado Obtenido '{}'", resultadoObtenido);
+		LOGGER.debug("Resultado Esperado '{}'", resultadoEsperado);
+		LOGGER.debug("Prueba '{}'", prueba);
+		LOGGER.debug("Tipo Prueba '{}'", tipoPrueba);
+		LOGGER.debug("Valor '{}'", valor);
+		LOGGER.debug("Campo '{}'", campo);
 
 		return datosPruebaAtributos;
 
